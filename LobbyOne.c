d5656b5d779e5eb20252769b4e384ee54f8f6401
Enjy Salem <esalem@barracuda.com>
2026-03-18 22:58:32 +0100

    Task3

---
+++ LobbyOne.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>   // for malloc
+#include <ctype.h>   // to use a function to check if char is a number
+
+#define FILENAME "LobbyBatteries.txt"
+
+int main()
+{
+    
+    FILE *fp = fopen(FILENAME, "r");
+    char *bank = NULL;
+
+    size_t bufferSize = 0;
+    int totalOutputJoltage = 0;
+
+    if(fp == NULL)
+    {
+        printf("File doesn't exist!\n");
+        return -1;
+    }
+
+    while(getline(&bank, &bufferSize, fp) != -1)
+    {
+        int maxBankJolts = 0;
+
+        char *ptrA;
+        char *ptrB;
+
+        for(ptrA = bank; *ptrA != '\0'; ptrA++)
+        {
+            if(!isdigit(*ptrA))
+            {
+                continue;
+            }
+
+            for(ptrB = ptrA +1; *ptrB != '\0'; ptrB++)
+            {
+                if(!isdigit(*ptrB))
+                {
+                    continue;
+                }
+
+                int current = (*ptrA - '0') * 10 + (*ptrB - '0');
+
+                if(current > maxBankJolts)
+                {
+                    maxBankJolts = current;
+                }
+            }
+        }
+
+        totalOutputJoltage += maxBankJolts;
+    }
+    
+    free(bank);
+    fclose(fp);
+    fp = NULL;
+
+    printf("The total output joltage is: %d\n", totalOutputJoltage);
+
+    return 0;
+}

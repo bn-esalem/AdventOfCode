bc5f9736477d29ecfe5e6fd0978849eab88ec62c
Enjy Salem <esalem@barracuda.com>
2026-03-19 13:07:38 +0100

    Task3

--- LobbyTwo.c
+++ LobbyTwo.c
@@ -8,10 +8,10 @@ int main()
 {
     
     FILE *fp = fopen(FILENAME, "r");
-    char *bank = NULL;
+    char *line = NULL;
 
     size_t bufferSize = 0;
-    int totalOutputJoltage = 0;
+    long long totalOutputJoltage = 0;
 
     if(fp == NULL)
     {
@@ -19,17 +19,56 @@ int main()
         return -1;
     }
 
-    while(getline(&bank, &bufferSize, fp) != -1)
+    while(getline(&line, &bufferSize, fp) != -1)
     {
-        
+        long long totalDigits = 0;
+
+        for(int i = 0; line[i] != '\0'; i++)
+        {
+            if(isdigit(line[i]))     // check if digit because sometimes there is ' ' or '/'
+            {
+                line[totalDigits ++] = line[i]; // to put only digits and then move to next array slot
+            }
+        }
+
+        line[totalDigits] = '\0';
+
+        if (totalDigits < 12)
+        {
+            continue;    // not break; because we just want to skip this line not to stop reading from file
+        }
+
+        char result [13] = "";
+        int currentPos = 0;  // where we standing on the line
+
+        for(int i = 0; i < 12; i++)
+        {
+            int remainingDigits = 11-i;
+            int maxIndex = currentPos;
+
+            for(int j = currentPos; j <= totalDigits - remainingDigits - 1; j++)
+            {
+                if(line[j] > line[maxIndex])
+                {
+                    maxIndex = j;
+                }
+            } 
+            
+            result[i] = line[maxIndex];
+            currentPos = maxIndex + 1;
+        }
+
+        result[12] = '\0';
+
+        totalOutputJoltage += atoll(result);
 
     }
 
-    free(bank);
+    free(line);
     fclose(fp);
     fp = NULL;
 
-    printf("The total output joltage is: %d\n", totalOutputJoltage);
+    printf("The total output joltage is: %lld\n", totalOutputJoltage);
 
     return 0;
 }
